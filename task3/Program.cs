d12d71888aff1a70b01b6ba284a6128db0851003
Tyapor718 <katepsupport@mail.com>
2023-04-07 22:32:19 +0400

    Задание массива нужной длины и заданных min max

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,12 +1,12 @@
 ﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 //1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 //6, 1, 33 -> [6, 1, 33]
-int[] creatArray()
+int[] creatArray(int len, int min, int max)
 {
-    int[] res = new int[8];
-    for (int i = 0; i < res.Length; i++)
+    int[] res = new int[len];
+    for (int i = 0; i < len; i++)
     {
-        res[i] = new Random().Next(0, 33);
+        res[i] = new Random().Next(min, max + 1);
     }
     return res;
 }
@@ -24,6 +24,10 @@ int ReadInt(string message)
     System.Console.Write($"{message} > ");
     return Convert.ToInt32(Console.ReadLine());
 }
-int[] mass = creatArray();
+int S = ReadInt("Введите длину массива");
+int MIN = ReadInt("Введите минимум");
+int MAX = ReadInt("Введите максимум");
+int[] mass = creatArray(S, MIN, MAX);
 PrintArray(mass);
+
 
